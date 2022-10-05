0aed5015dc0b087e80f0f62954eacbfc180223ee
Rogel <106194161+legorll@users.noreply.github.com>
2022-10-05 16:56:17 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -3,7 +3,7 @@
 // console.log("Eu estou no console.");
 let tabuada = 5;
 function alo(){
-    
+
     let tabuada = document.getElementById("entrada").value;
 
     document.write("<h1>Tabuada do " + tabuada + "</h1>");
@@ -32,4 +32,11 @@ function quadrado(){
     for(var i = 2; i < 11; i++){
         document.write("O quadrado de" + i + " é " + (i+i)+"<br>");
     }
+}
+
+function total(){
+    let val = document.getElementById("valor").value;
+    let ju = document.getElementById("juros").value;
+    let resultado = (val * (ju/100) +1) + val;
+    document.write("O total é de: " + resultado)
 }
