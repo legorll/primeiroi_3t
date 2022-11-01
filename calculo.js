7e9500c62a6fe2a0ae6df0ecd957f0a97a88c451
Rogel <106194161+legorll@users.noreply.github.com>
2022-11-01 17:40:15 -0300

    Create calculo.js

---
+++ calculo.js
@@ -0,0 +1,17 @@
+let a = 6;
+let b = -5;
+let c = -1;
+let delta = (b*b)-4*a.c;
+if(delta < 0){
+    console.log("Não tem raíz real");
+}
+if(delta == 0){
+    let x1 = (-b)/(2*a);
+    let x2 = x1;
+    console.log("x1 = x2 = " + x1);
+}
+if (delta > 0{
+    let x1 = (-b) + Math.sqrt(delta))/(2*a);
+    let x2 = (-b) - Math.sqrt(delta))/(2*a);
+    console.log("x1 = " + x1 + " x2 = " + x2);
+}
